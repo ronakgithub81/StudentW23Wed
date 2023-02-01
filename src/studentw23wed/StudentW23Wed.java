3f188f01efc7c01eae2a2977783bbddc436eff7a
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-02-01 16:03:27 -0500

    added new attribute student address

--- src/studentw23wed/StudentW23Wed.java
+++ src/studentw23wed/StudentW23Wed.java
@@ -8,12 +8,21 @@ package studentw23wed;
 public class StudentW23Wed {
     private String studentId;
     private String studentName;
+    private String studentAddress;
 
     public StudentW23Wed(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
     }//end of constructor
 
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+    
     public String getStudentId() {
         return studentId;
     }
